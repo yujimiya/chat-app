21e8029b15279be1af860d06c789da8fcaf220e1
yujimiya <yuji.miyamoto32@gmail.com>
2023-10-01 17:21:13 +0900

    messageテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,9 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
-
-  validates :name, presence: true
+  has_many :messages
   
+  validates :name, presence: true
+
 end
 
